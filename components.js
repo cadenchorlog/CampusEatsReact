8395a2e059e93c6ed7e511f8d2bcc6946d5b88ee
cadenchorlog <53951245+cadenchorlog@users.noreply.github.com>
2022-08-31 13:48:57 -0600

    notifications

--- components.js
+++ components.js
@@ -4,6 +4,86 @@
 // 'react' is required.
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+    //this.setState({ expoPushToken: token });
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#D22F27',
+    });
+  }
+
+  return token;
+}
+
+export function PushNotifications() {
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener(notification => {
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener(response => {
+        console.log(response);
+      });
+
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current
+      );
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+      }}
+    >
+      <Text>Token: {expoPushToken}</Text>
+      <Text>Title: {notification && notification.request.content.title}</Text>
+      <Text>Body: {notification && notification.request.content.body}</Text>
+      <Text>
+        Data:{' '}
+        {notification && JSON.stringify(notification.request.content.data)}
+      </Text>
+    </View>
+  );
+}
 
 export {
   requestForegroundPermissionsAsync,
